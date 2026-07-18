202ee4c4fd42f047dc7b3486ea1db09f3ea5ff75
Brian Ventura <briandagoberto20@hotmail.com>
2026-07-18 08:16:56 -0600

    dd

--- app/helpers.php
+++ app/helpers.php
@@ -1570,7 +1570,7 @@ if (!function_exists('fex')) {
         $comprobante["resumen"]                  = $resumen;
         //$comprobante["extension"]                = $extension;
         $comprobante["apendice"]                 = $apendice;
-        //dd($comprobante);
+        dd($comprobante);
         return ($comprobante);
     }
 }
@@ -2094,7 +2094,7 @@ if (!function_exists('fse')) {
         $comprobante["resumen"]                  = $resumen;
         $comprobante["apendice"]                 = $apendice;
         //$comprobante2 = [];
-        dd($comprobante);
+        //dd($comprobante);
         return ($comprobante);
     }
 }
